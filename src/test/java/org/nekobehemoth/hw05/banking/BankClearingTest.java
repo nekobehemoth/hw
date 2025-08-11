598c258f6d6231f15708f03afcb9ed4a60cc679e
ushakofuiria <Ilya.Ushakov@lotusretails.com>
2025-08-11 20:31:09 +0700

    add the widthdrawal and deposit tests

--- src/test/java/org/nekobehemoth/hw05/banking/BankClearingTest.java
+++ src/test/java/org/nekobehemoth/hw05/banking/BankClearingTest.java
@@ -1,14 +1,13 @@
 package org.nekobehemoth.hw05.banking;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankClearingTest {
 
@@ -41,6 +40,70 @@ public class BankClearingTest {
         Client client = new Client();
         assertTrue(client.bankClearing(bankImpl));
     }
+
+    @ParameterizedTest(name = "Client for {0} bank should return true when transfer money with threads")
+    @MethodSource("bankImplementation")
+    void testSimultaneousWithdrawal(String implName, Bank bankImpl) throws Exception {
+        ThreadFactory threadFactory = Thread.ofVirtual().factory();
+        long balance_before = bankImpl.getAccountBalance(1);
+        Thread t1 = threadFactory.newThread(() -> {
+            try {
+                bankImpl.withdraw(1, 10);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread t2 =  threadFactory.newThread(() -> {
+            try {
+                bankImpl.withdraw(1, 10);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        long balance_after = bankImpl.getAccountBalance(1);
+
+
+        assertEquals(20, balance_before - balance_after);
+    }
+
+    @ParameterizedTest(name = "Client for {0} bank should return true when transfer money with threads")
+    @MethodSource("bankImplementation")
+    void testSimultaneousDeposit(String implName, Bank bankImpl) throws Exception {
+        ThreadFactory threadFactory = Thread.ofVirtual().factory();
+        long balance_before = bankImpl.getAccountBalance(1);
+        Thread t1 = threadFactory.newThread(() -> {
+            try {
+                bankImpl.deposit(1, 10);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread t2 =  threadFactory.newThread(() -> {
+            try {
+                bankImpl.deposit(1, 10);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        long balance_after = bankImpl.getAccountBalance(1);
+
+
+        assertEquals(20, balance_after - balance_before);
+    }
+
+
 
 
 }
